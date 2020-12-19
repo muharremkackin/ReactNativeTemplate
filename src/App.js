4a0669794651f79aa1089f854e7c78ecef12f136
Muharrem Kaçkın <muharremkackin@gmail.com>
2020-12-19 19:32:49 +0300

    configured for usuablity

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Welcome to React Native Template</Text>
+    </View>
+  );
+}
+
+export default App;
